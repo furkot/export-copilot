62bbbdb877f3b3fa97e0e4a216c6d089a36f793f
Natalia Kowalczyk <melitele@code42day.com>
2015-02-20 15:44:18 -0500

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,15 @@
+#furkot-copilot
+
+Generate copilot .TRP files from [Furkot] trip data
+
+# Usage
+
+Install the tools using npm
+
+    npm install
+
+## License
+
+MIT
+
+[Furkot]:https://trips.furkot.com
---
+++ test/trp.test.js
@@ -0,0 +1,45 @@
+var assert = require('assert');
+var trp = require('../lib/trp');
+var fs = require('fs');
+var path = require('path');
+
+function loadFile(dirname, file) {
+  var filename = path.resolve(dirname, file);
+  return fs.readFileSync(filename);
+}
+
+function loadJson(dirname, file) {
+  return JSON.parse(loadFile(dirname, file));
+}
+
+describe('copilot trp', function () {
+  it('simple trip', function (done) {
+    var t = loadJson(__dirname, './fixtures/simple-trip.json'),
+      generated = trp(t, {
+        tracks: true,
+        creator: 'Furkot - road trip planner. See: https://trips.furkot.com',
+        destination: 'Furkot trip'
+      }),
+      expected = loadFile(__dirname, './fixtures/simple.trp');
+
+    // require('fs').writeFileSync('simple.trp', generated);
+
+    generated.should.eql(expected);
+    done();
+  });
+
+  it('multi trip', function (done) {
+    var t = loadJson(__dirname, './fixtures/multi-trip.json'),
+      generated = trp(t, {
+        tracks: true,
+        creator: 'Furkot - road trip planner. See: https://trips.furkot.com',
+        destination: 'Furkot trip'
+      }),
+      expected = loadFile(__dirname, './fixtures/multi.trp');
+
+    // require('fs').writeFileSync('multi.trp', generated);
+
+    generated.should.eql(expected);
+    done();
+  });
+});
